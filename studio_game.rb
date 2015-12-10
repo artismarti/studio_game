5e3cdf8cff49ba9a4f94d3f0ef2bcad8920720af
Arti Mathanda <artismarti@gmail.com>
2015-12-10 23:30:14 +0000

    Uses attributes. attr_reader attr_writer attr_accessor

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,16 @@
 class Player
+
+	attr_accessor :name
+	attr_reader :health
+
+
 	def initialize(name, health=100)
 		@name = name.capitalize
 		@health = health
 	end
 
 	def to_s
-		"I'm #{@name} with a health of #{@health}"
+		"I'm #{@name} with a health of #{@health} and a score of #{score}."
 	end
 
 	def blam
@@ -18,6 +23,10 @@ class Player
 		puts "#{@name} got w00ted!"
 	end
 
+	def score
+		@health + @name.length
+	end
+
 end
 
 player1 = Player.new("moe")
@@ -25,13 +34,13 @@ puts player1
 
 
 player2 = Player.new("larry", 60)
-puts player2
 player2.blam
 player2.w00t
 puts player2
+player2.name = "Lawrence"
+puts player2.name
 
 player3 = Player.new("curly", 125)
-puts player3
 puts player3.blam
 puts player3
 puts player3.w00t
