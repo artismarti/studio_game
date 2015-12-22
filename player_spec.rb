396e3924da2d496695cd93ac592f526dc2e1d68b
Arti Mathanda <artismarti@gmail.com>
2015-12-22 22:16:29 +0000

    Adds unit tests.

---
+++ player_spec.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+require_relative 'player'
+
+describe Player do
+  before do
+    $stdout = StringIO.new
+    @initial_health = 150
+    @player = Player.new("arti", @initial_health)
+
+  end
+  it "has a capitalized name" do
+    expect(@player.name).to eq("Arti")
+  end
+  
+  it "has an initial health" do
+    expect(@player.health).to eq(150)
+  end
+  
+  it "has a string representation" do
+    expect(@player.to_s).to eq("I'm Arti with a health of 150 and a score of 154.")
+  end
+  
+  it "computes a score as the sum of its health and length of name" do
+    expect(@player.score).to eq(154)
+
+  end
+  
+  it "increases health by 15 when w00ted" do
+    @player.w00t
+
+    expect(@player.health).to eq(@initial_health + 15)
+  end
+  
+  it "decreases health by 10 when blammed" do
+    @player.blam
+
+    expect(@player.health).to eq(@initial_health - 10)
+  end
+  
+end
