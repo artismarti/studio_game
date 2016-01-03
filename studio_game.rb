2dadeb390b316a5df9a2a82bc381aed4f3d96cf9
Arti Mathanda <artismarti@gmail.com>
2016-01-03 00:25:11 +0000

    Adds more players, prints stats, randomises health

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,22 @@
 require_relative 'game'
 
 player1 = Player.new("moe")
-player2 = Player.new("larry", 60)
-player3 = Player.new("curly", 125)
+player2 = Player.new("larry", rand(50..120))
+player3 = Player.new("curly", rand(50..120))
+player4 = Player.new("foo", rand(50..120))
+player5 = Player.new("bar", rand(50..120))
+player6 = Player.new("arti", rand(50..120))
+player7 = Player.new("smarti", rand(50..120))
+player8 = Player.new("sue", rand(50..120))
 
 knuckleheads = Game.new("knuckleheads")
 knuckleheads.add_player(player1)
 knuckleheads.add_player(player2)
 knuckleheads.add_player(player3)
-knuckleheads.play
+knuckleheads.add_player(player4)
+knuckleheads.add_player(player5)
+knuckleheads.add_player(player6)
+knuckleheads.add_player(player7)
+knuckleheads.add_player(player8)
+knuckleheads.play(10)
+knuckleheads.print_stats
