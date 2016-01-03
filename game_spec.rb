93fbca01023129dc44b6302ba2e7767833e12562
Arti Mathanda <artismarti@gmail.com>
2016-01-03 00:24:45 +0000

    Updates tests.

--- game_spec.rb
+++ game_spec.rb
@@ -7,25 +7,26 @@ describe Game do
 
     @initial_health = 100
     @player = Player.new("moe", @initial_health)
-
     @game.add_player(@player)
     allow_any_instance_of(Die).to receive(:roll).and_return(5)
   end
 
   it "skips the player if a medium number is rolled" do
     allow_any_instance_of(Die).to receive(:roll).and_return(3)
-
-    @game.play
-
+    @game.play(2)
     expect(@player.health).to eq(@initial_health)
   end
 
   it "blams the player if a low number is rolled" do
     allow_any_instance_of(Die).to receive(:roll).and_return(1)
+    @game.play(2)
+    expect(@player.health).to eq(@initial_health - (10*2))
+  end
 
-    @game.play
-
-    expect(@player.health).to eq(@initial_health - 10)
+  it "w00ts the player if a high number is rolled" do
+    allow_any_instance_of(Die).to receive(:roll).and_return(6)
+    @game.play(2)
+    expect(@player.health).to eq(@initial_health + (15*2))
   end
 
 
