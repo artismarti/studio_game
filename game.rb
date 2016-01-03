2616292ad2d58bea6d112305e52d99818b6a4aa6
Arti Mathanda <artismarti@gmail.com>
2016-01-03 00:24:37 +0000

    Defines num of rounds. Creates stats method.

--- game.rb
+++ game.rb
@@ -14,12 +14,29 @@ class Game
     @players.push(player)
   end
 
-  def play
+  def play(rounds)
     puts "There are #{@players.size} players in #{@title}:"
 
-      @players.each do |player|
-        GameTurn.take_turn(player)
-        puts player
+      1.upto(rounds) do |round|
+        puts "\nRound #{round}:"
+        @players.each do |player|
+          GameTurn.take_turn(player)
+          puts player
+        end
       end
+  end
+
+  def print_stats
+    strong_players, weak_players = @players.partition{|player| player.strong?}
+    puts "#{@title} Stats:\n"
+    puts "#{strong_players.length} Strong Player(s)\n"
+    strong_players.each do |player|
+      puts "#{player.name} (#{player.health})"
+    end
+
+    puts "#{weak_players.length} Weak Player(s)\n"
+    weak_players.each do |player|
+      puts "#{player.name} (#{player.health})"
+    end
   end
 end
