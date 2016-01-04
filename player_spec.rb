6cdf700a8326f3649271c197b005be0bbb94dba7
Arti Mathanda <artismarti@gmail.com>
2016-01-04 09:38:27 +0000

    Adds tests for point accumulation.

--- player_spec.rb
+++ player_spec.rb
@@ -37,6 +37,16 @@ describe Player do
     expect(@player.health).to eq(@initial_health - 10)
   end
 
+  it "computes points as the sum of all treasure points" do
+    expect(@player.points).to eq(0)
+    @player.found_treasure(Treasure.new(:hammer, 50))
+    expect(@player.points).to eq(50)
+    @player.found_treasure(Treasure.new(:crowbar, 400))
+    expect(@player.points).to eq(450)
+    @player.found_treasure(Treasure.new(:hammer, 50))
+    expect(@player.points).to eq(500)
+  end
+
   context "with health greater than 100" do
     before do
       @player = Player.new("larry", 150)
