1a4d162cf56d7047f033b6b629fa286dc6e2f147
Arti Mathanda <artismarti@gmail.com>
2016-01-04 23:49:18 +0000

    Adds tests for treasure points.

--- game_spec.rb
+++ game_spec.rb
@@ -29,5 +29,11 @@ describe Game do
     expect(@player.health).to eq(@initial_health + (15*2))
   end
 
-
+  it "assigns a treasure for points during a player's turn" do
+    game = Game.new("Knuckleheads")
+    player = Player.new("sue")
+    game.add_player(player)
+    game.play(1)
+    expect(player.points).not_to be_zero
+  end
 end
